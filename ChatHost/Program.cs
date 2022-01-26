3467c2048b469fb05ece47a9251fa507fb29793a
Elli <78215127+ElliNow@users.noreply.github.com>
2022-01-26 20:13:32 +0300

    commit

--- ChatHost/Program.cs
+++ ChatHost/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace wcfChat
+namespace ChatHost
 {
-    public class Class1
+    class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }
---
+++ wcfChat/ServiceChat.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace wcfChat
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class ServiceChat : IServiceChat
+    {
+        List<ServerUser> users = new List<ServerUser>();
+        int nextId = 1;
+        public int Connect(string name)
+        {
+            ServerUser user = new ServerUser()
+            {
+                Id = nextId,
+                Name = name,
+                operationContext = OperationContext.Current
+            };
+            nextId++;
+            SendMessage($"{user.Name} Подключился к чату!",0);
+            users.Add(user);
+            return user.Id;
+        }
+
+        public void Disconnect(int id)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+            if(user != null)
+            {
+                users.Remove(user);
+                SendMessage($"{user.Name} Покинул чат!",0);
+            }
+        }
+
+        public void SendMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
